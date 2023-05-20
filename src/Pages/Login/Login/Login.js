223482af7ef5b6def90e898481c278598ce30ddb
Nafis Hasnat <nafis7823@gmail.com>
2023-05-20 13:37:40 +0600

    firebase authentication added

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,9 +1,52 @@
-import React from 'react';
-
+import React, { useContext } from 'react';
+import { useForm } from "react-hook-form";
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../contexts/AuthProvider';
 const Login = () => {
+    const { register, formState: { errors }, handleSubmit } = useForm();
+    const { signIn, setLoading, setUser } = useContext(AuthContext);
+
+    const handleLogin = data => {
+        console.log(data);
+        signIn(data.email, data.password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                setUser(user);
+                setLoading(false);
+            })
+            .catch(err => console.log(err))
+    }
+
     return (
-        <div>
-            <h2>This is login</h2>
+        <div className='h-[700px] flex justify-center items-center'>
+            <div className='w-96 p-7'>
+                <h2 className="text-xl text-center font-bold">Login</h2>
+                <form onSubmit={handleSubmit(handleLogin)}>
+                    <div className="from-control w-full">
+                        <label className="label"><span className='label-text'>Email</span></label>
+                        <input type='email'
+                            {...register("email", { required: 'Email is required' })}
+                            className='input input-primary input-bordered w-full' />
+                        {errors.email && <p className='text-red-600' role="alert">{errors.email?.message}</p>}
+                    </div>
+                    <div className="from-control w-full">
+                        <label className="label"><span className='label-text'>Password</span></label>
+                        <input type='password'
+                            {...register("password", {
+                                required: "Password is required",
+                                minLength: { value: 6, message: 'Password must be at least 6 characters or longer' },
+                            })}
+                            className='input input-primary input-bordered w-full' />
+                        {errors.password && <p className='text-red-600' role="alert">{errors.password?.message}</p>}
+                    </div>
+                    <label className="label"><span className='label-text'>Forget Password?</span></label>
+                    <input className='btn btn-primary w-full' type="submit" value='Login' />
+                </form>
+                <p>New to Doctors Portal? <Link to='/register' className='text-blue-600 font-bold'>Create new account</Link> </p>
+                <div className="divider">OR</div>
+                <button className="btn btn-accent w-full">Continue with Google</button>
+            </div>
         </div>
     );
 };
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,9 +1,64 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../contexts/AuthProvider';
 
 const Register = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const { setUser, setLoading, createUser } = useContext(AuthContext);
+
+    const handleSignUp = data => {
+        createUser(data.email, data.password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                setUser(user);
+                setLoading(false);
+            })
+            .catch(err => console.log(err))
+    }
+
     return (
-        <div>
-            <h2>This is register!</h2>
+        <div className='h-[700px] flex justify-center items-center'>
+            <div className='w-96 p-7'>
+                <h2 className="text-xl text-center font-bold">Sign Up</h2>
+                <form onSubmit={handleSubmit(handleSignUp)}>
+                    <div className="from-control w-full">
+                        <label className="label"><span className='label-text'>Name</span></label>
+                        <input type='text'
+                            {...register('name', {
+                                required: 'Name is required'
+                            })}
+                            className='input input-primary input-bordered w-full' />
+                        {errors.name && <span className='text-red-600'>{errors.name?.message}</span>}
+                    </div>
+                    <div className="from-control w-full">
+                        <label className="label"><span className='label-text'>Email</span></label>
+                        <input type='email'
+                            {...register('email', {
+                                required: 'Email is required!'
+                            })}
+                            className='input input-primary input-bordered w-full' />
+                        {errors.email && <span className='text-red-600'>{errors.email?.message}</span>}
+                    </div>
+                    <div className="from-control w-full">
+                        <label className="label"><span className='label-text'>Password</span></label>
+                        <input type='password'
+                            {...register('password', {
+                                required: 'Password is required',
+                                minLength: { value: 6, message: 'Password must be at least 6 characters or longer' },
+                                // pattern: { value: /^(?=(.*[a-z]){1})(?=(.*[A-Z]){1})(?=(.*[0-9]){1})(?=(.*[!@#$%^&*()\-__+.]){1}).{6}$/, message: "Password must be strong" }
+                            })}
+                            className='input input-primary input-bordered w-full' />
+                        {errors.password && <span className='text-red-600'>{errors.password?.message}</span>}
+                    </div>
+                    <input className='btn btn-primary w-full mt-6' type="submit" value='Sign Up' />
+                </form>
+                <p>Already have an account? <Link to='/login' className='text-blue-600 font-bold'>Please Login</Link> </p>
+                <div className="divider">OR</div>
+                <button className="btn btn-accent w-full">Continue with Google</button>
+            </div>
         </div>
     );
 };
